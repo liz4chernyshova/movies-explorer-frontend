6c853e066ca207d36ad5305b62a8b904a4400628
Liza <liz4chernyshova@yandex.ru>
2021-11-18 23:59:08 +0300

    ref: 2nd review

--- src/components/SavedMoviesCard/SavedMoviesCard.js
+++ src/components/SavedMoviesCard/SavedMoviesCard.js
@@ -4,12 +4,13 @@ import '../MoviesCard/MoviesCard.css';
 
 function SavedMoviesCard(props) {
 
-   
     return (
         <div className="card">
           <div className="card__info">
-            <h2 className="card__title">{props.card.name}</h2>
-            <p className="card__time">{props.card.time}</p>
+            <article className="card__text">
+              <h2 className="card__title">{props.card.name}</h2>
+              <p className="card__time">{props.card.time}</p>
+              </article>
             <button className='card__button_type_delete'></button>
           </div>
           <img className="card__image" src={props.card.img} alt={props.card.name} />
